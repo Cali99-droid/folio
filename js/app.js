33e0901e72ffc17e8fab5c4ad2bfb26f5eedc24d
Carlos Orellano <orellano428@gmail.com>
2022-12-17 01:19:00 -0500

    mejoras en inicio

---
+++ js/app.js
@@ -0,0 +1,29 @@
+eventListeners();
+function eventListeners() {
+  var typed = new Typed("#typed", {
+    stringsElement: "#typed-strings",
+    backSpeed: 40,
+    typeSpeed: 40,
+    loop: true,
+  });
+}
+
+$(document).ready(function () {
+  irArriba();
+}); //Hacia arriba
+
+function irArriba() {
+  $(".ir-arriba").click(function () {
+    $("body,html").animate({ scrollTop: "0px" }, 1000);
+  });
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 0) {
+      $(".ir-arriba").slideDown(600);
+    } else {
+      $(".ir-arriba").slideUp(600);
+    }
+  });
+  $(".ir-abajo").click(function () {
+    $("body,html").animate({ scrollTop: "1000px" }, 1000);
+  });
+}
